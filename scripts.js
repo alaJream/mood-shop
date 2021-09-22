7ab0d7ba10df5bd1cd27dc22c91cd4f03f88153d
alaJream <jeremy.gonzales@students.dominican.edu>
2021-09-22 10:22:06 -0700

    add button connected to cart

--- scripts.js
+++ scripts.js
@@ -42,6 +42,12 @@ for (let i = 0; i < data.length; i += 1) {
 	button.innerHTML = "Add to Cart"
 	newDiv.appendChild(button)
 }
+const all_items_button = Array.from(document.querySelectorAll("button"))
+all_items_button.forEach(elt => elt.addEventListener('click', () => {
+    addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
+    showItems()
+  }))
+
 const cart = []
 
 // add item
