fc5e577825236a5cb9bb6452ca15df717f66a699
alaJream <jeremy.gonzales@students.dominican.edu>
2021-09-22 00:42:52 -0700

    connect cart to footer

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+const itemList = document.getElementById('item-list')
+const cartQty = document.getElementById('cart-qty')
+const cartTotal = document.getElementById('cart-total')
+
+itemList.innerHTML = '<li> Hello World</li>'
+
 const itemsContainer = document.querySelector('#items')
 
 import data from './data.js'
@@ -36,11 +42,72 @@ for (let i = 0; i < data.length; i += 1) {
 	button.innerHTML = "Add to Cart"
 	newDiv.appendChild(button)
 }
-const cart = [ ]
-function addItem() {
+const cart = []
+
+// add item
+function addItem(name, price) {
+   for (let i = 0; i < cart.length; i += 1) {
+       if (cart[i].name === name) {
+           cart[i].qty += 1
+           return      
+        }
+   }
+
+   const item = { name, price, qty: 1 }
+   cart.push(item)
 
 }
 
+// show item
 function showItems() {
-    
+    const qty = getQty
+  //  console.log(`You have ${qty} items in your cart`)
+    cartQty.innerHTML = `You have ${qty} items in your cart`
+
+    let itemStr = ''
+    for (let i = 0; i < cart.length; i+= 1) {
+       // console.log(`- ${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+        const { name, price, qty } = cart[i]
+       
+        itemStr += `<li> 
+          ${name} 
+          $${price} x ${qty} = 
+          ${qty * price} </li>`
+    }
+    itemList.innerHTML = itemStr
+
+   // console.log(`Total in cart: $${getTotal()}`)
+    cartTotal.innerHTML = `Total in cart: $${getTotal()}`
+}
+
+// get qty
+function getQty() {
+    let qty = 0
+    for (let i = 0; i <cart.length; i+= 1) {
+        qty += cart[i].qty
+    }
+    return qty
+}
+
+// get total
+function getTotal() {
+    let total = 0
+    for (let i = 0; i < cart.length; i += 1) {
+        total += cart[i].price * cart[i].qty
+    }
+}
+
+
+// remove item
+function removeItem(name, qty = 0) {
+    for (let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name)
+            if (qty > 0) {
+                cart[i].qty -= qty
+            }
+            if (cart[i].qty < 1 || qty === 0){
+                cart.splice(i, 1)
+            }
+            return
+    }
 }
